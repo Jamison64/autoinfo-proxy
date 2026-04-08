8ea82f01bb9859ec6cee53ed0089b2fccd176dff
Jamison64 <jzshah@thecurve.com.au>
2026-04-08 13:44:28 +1000

    add IP endpoint

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const http = require("http");
+const https = require("https");
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ status: "AutoInfo Proxy Running" }));
+  if (req.url === "/myip") {
+    https.get("https://api.ipify.org?format=json", (apiRes) => {
+      let data = "";
+      apiRes.on("data", (chunk) => (data += chunk));
+      apiRes.on("end", () => {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(data);
+      });
+    });
+  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ status: "AutoInfo Proxy Running" }));
+  }
 });
 
 server.listen(PORT, () => {
