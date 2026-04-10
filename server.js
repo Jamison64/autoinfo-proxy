d61161b44c75c758216851eaaf88f65489af6a7e
Jamison64 <jzshah@thecurve.com.au>
2026-04-10 10:08:27 +1000

    add /makes route for VehicleQueryD

--- server.js
+++ server.js
@@ -3,7 +3,54 @@ const https = require("https");
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+const USER_ID = "17292";
+const AUTH_CODE = "LY9-P98-KK2";
+
+function getMakes() {
+  return new Promise((resolve, reject) => {
+    const soapBody = `<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <soap:Body>
+    <VehicleQueryD xmlns="http://autoi.com.au/">
+      <UserId>${USER_ID}</UserId>
+      <AuthCode>${AUTH_CODE}</AuthCode>
+      <UserCookie></UserCookie>
+      <MakeId>0</MakeId>
+      <ModelId>0</ModelId>
+      <YearId>0</YearId>
+      <SeriesId>0</SeriesId>
+      <BodyId>0</BodyId>
+      <EngineId>0</EngineId>
+      <AU>true</AU>
+      <NZ>true</NZ>
+    </VehicleQueryD>
+  </soap:Body>
+</soap:Envelope>`;
+
+    const options = {
+      hostname: "test.autoinfo.com.au",
+      path: "/API/AutoInfoGateway.asmx",
+      method: "POST",
+      headers: {
+        "Content-Type": "text/xml; charset=utf-8",
+        SOAPAction: "http://autoi.com.au/VehicleQueryD",
+        "Content-Length": Buffer.byteLength(soapBody),
+      },
+    };
+
+    const req = http.request(options, (apiRes) => {
+      let data = "";
+      apiRes.on("data", (chunk) => (data += chunk));
+      apiRes.on("end", () => resolve(data));
+    });
+
+    req.on("error", (err) => reject(err));
+    req.write(soapBody);
+    req.end();
+  });
+}
+
+const server = http.createServer(async (req, res) => {
   if (req.url === "/myip") {
     https.get("https://api.ipify.org?format=json", (apiRes) => {
       let data = "";
@@ -13,6 +60,15 @@ const server = http.createServer((req, res) => {
         res.end(data);
       });
     });
+  } else if (req.url === "/makes") {
+    try {
+      const xml = await getMakes();
+      res.writeHead(200, { "Content-Type": "application/xml" });
+      res.end(xml);
+    } catch (err) {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: err.message }));
+    }
   } else {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ status: "AutoInfo Proxy Running" }));
